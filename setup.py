f0a4475042547804638bb52af3f2061092d32afa
ryuku <aze@glk.com>
2013-03-08 14:34:56 +0100

    added numpy include dirs

--- setup.py
+++ setup.py
@@ -4,11 +4,13 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import numpy 
 
 ext_modules = [Extension("warps", ["warps.pyx"])]
 
 setup(
   name = 'Hello world app',
   cmdclass = {'build_ext': build_ext},
+  include_dirs = [numpy.get_include()],
   ext_modules = ext_modules
 )
