15c1f6e0a69c0040270083ccc6e03867beb2c609
Bartosz Telenczuk <muchatel@poczta.fm>
2013-03-17 00:51:43 +0100

    added subdirs in batch processing script

--- batch_transform.py
+++ batch_transform.py
@@ -19,20 +19,35 @@ if __name__ == '__main__':
 
     transforms = np.recfromcsv(fname, names=None)
 
-    imgs = {}
+    imgs = []
 
     for t in transforms:
         data = list(t)
         date, target, ref = data[:3]
 
-        imgs[target] = data[3:]
+        imgs.append((target, ref, data[3:]))
 
 
-    for img, params in imgs.items():
+    for img, ref, params in imgs:
         im = io.imread(os.path.join(path_src,img))
         matrix = np.array(params).reshape(3,3)
         trans = transform.AffineTransform(matrix=matrix)
         img_reshaped = warp_int(im, trans.inverse)
-        io.imsave(os.path.join(path_target,img), img_reshaped)
-        print "Processed image %s" % img
+        
+        save_dir = os.path.join(path_target, ref)
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+
+        file_path = os.path.join(save_dir, img)
+        i = 1
+        
+        while os.path.exists(file_path):
+            root, ext = os.path.splitext(img)
+            img_name = "".join([root , "_reg%d" % i, ext])
+
+            file_path = os.path.join(save_dir, img_name)
+            i+=1
+
+        io.imsave(file_path, img_reshaped)
+        print "Processed image %s and saved to %s" % (img, file_path)
 
